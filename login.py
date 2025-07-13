439e8e3a9acca67c17ca82b0b3478ddcf7e98982
Ramprakash-2008 <rramp762008@gmail.com>
2025-07-13 14:18:08 +0530

    Update login.py

--- login.py
+++ login.py
@@ -294,6 +294,6 @@ def logout():
 
 # === Main ===
 if __name__ == "__main__":
-    init_db()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
 
